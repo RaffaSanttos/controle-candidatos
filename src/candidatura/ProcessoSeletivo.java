c81214e59ea15990195bb5770535bc540faf73eb
RaffaSanttos <atomix.luke@gmail.com>
2024-11-11 16:23:34 -0300

    Eliminando blocos inutilizados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -30,15 +30,4 @@ public class ProcessoSeletivo {
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
-
-    static void analisarCandidato(double salarioPretendido) {
-        double salarioBase = 2000.0;
-        if(salarioBase > salarioPretendido) {
-            System.out.println("Ligar para o candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Ligar para o candidato com contra-proposta");
-        } else {
-            System.out.println("Aguardando o resultado dos demais candidatos");
-        }
-    }
 }
